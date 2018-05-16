9a95172732b94ae4e6ec1c551cc4039d24055b4f
Dennis <bereznd1@tcnj.edu>
2018-05-16 18:59:30 -0400

    homepage

--- schema.sql
+++ schema.sql
@@ -1,4 +1,52 @@
--- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS blogger;
--- Creates the "blogger" database --
-CREATE DATABASE blogger;
+create database ticketDon;
+
+use ticketDon;
+
+create table users_givers(
+username varchar(25) not null,
+password varchar(25) not null, 
+street_address varchar(225) not null, 
+city varchar(25) not null,
+state varchar(25) not null,  
+zip_code varchar(25) not null, 
+phone  integer(20) not null, 
+email varchar(35) not null, 
+primary key (username)
+);
+
+create table users_recepients(
+username varchar(25) not null,
+password varchar(25) not null, 
+street_address varchar(225) not null, 
+city varchar(25) not null,
+state varchar(25) not null,  
+zip_code varchar(25) not null, 
+phone  integer(20) not null, 
+email varchar(35) not null,
+aarp_num varchar (40) not null, 
+primary key (username)
+);
+
+create table events(
+event_id Int( 11 ) AUTO_INCREMENT NOT NULL,
+event_name varchar(25) not null,
+event_street_address varchar(225) not null, 
+event_city varchar(25) not null,
+event_state varchar(25) not null,  
+event_zip_code varchar(25) not null,
+primary key (event_id)
+);
+
+create table tickets(
+donation_id Int( 11 ) AUTO_INCREMENT NOT NULL,
+event_id Int( 11 ) NOT NULL,
+event_street_address varchar(225) not null, 
+event_city varchar(25) not null,
+event_state varchar(25) not null,  
+event_zip_code varchar(25) not null,
+primary key (donation_id)
+);
+
+ALTER TABLE tickets 
+ADD CONSTRAINT FK_events 
+FOREIGN KEY (event_id) REFERENCES events(event_id);
