5423ccb3bf6e04ec2fc347ae14b252901c7b50b4
Dennis <bereznd1@tcnj.edu>
2018-05-20 18:23:16 -0400

    more front end work

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,22 +9,34 @@ var path = require("path");
 // Routes
 // =============================================================
 module.exports = function(app) {
-
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+    res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 
-  // Route to the cms page
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/about.html"));
   });
 
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+  app.get("/apply", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/apply.html"));
+  });
+
+  app.get("/donate", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/donate.html"));
+  });
+
+  app.get("/donorhome", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/donorhome.html"));
+  });
+
+  app.get("/recipienthome", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/recipienthome.html"));
+  });
+
+  app.get("/newEvents", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/newEvents.html"));
   });
 
 };
