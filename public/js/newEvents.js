87b40496c52a61e8b6c041138d46cdfead9e57b4
Matt M <matthewmccarthy2@gmail.com>
2018-05-22 15:50:07 -0400

    updated mySQL data table/ added js for newEvents submit form

---
+++ public/js/newEvents.js
@@ -0,0 +1,119 @@
+// TICKETMASTER API AND AJAX CALL =========================
+
+var searchCounter = 0;
+var queryTerm = "";
+  var queryURLBase = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=NdH7ttoqHEznKuGMVdBINJqbG8r9w1Kk&keyword=";
+
+$("#search-events-btn").on("click", function(event) {
+      event.preventDefault();
+
+$(".panel-primary").attr("style", "display: block;");
+
+      queryTerm = $("#queryTerm").val().trim();
+      console.log(queryTerm);
+
+      var queryURL = queryURLBase + queryTerm;
+      console.log(queryURL); 
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    console.log(response);       
+
+
+// Display SEARCH RESULTS into a TABLE =========
+
+    var events = response._embedded.events; 
+    console.log(events); 
+
+    $("#event-table > tbody").empty();
+    // $("#search-events-form").empty();
+
+    for (var i = 0; i < events.length; i++) {          
+      
+
+      var eventName = events[i].name;             
+     
+      var location = events[i]._embedded.venues[0].city.name;
+                        
+      var date = events[i].dates.start.localDate;
+      
+      var venue = events[i]._embedded.venues[0].name; 
+
+      var url = events[i].url;
+                
+
+      $("#event-table > tbody").append("<tr><td>" + eventName + "</td><td>" + location + "</td><td>" +
+      date + "</td><td>" + venue + "</td><td><a target='_blank' href='" + url + "'>more info</a></td></tr>");        
+
+    };
+
+      $.ajax({
+    url: "/api/artist",
+    method: "GET",
+    data: {
+      artist: queryTerm
+    }
+  }).done(function(response) {
+    console.log(response);
+  })
+})
+});
+
+// Submit Form
+
+$(document).ready(function () {
+
+  $("#newOpportunityForm").on("submit", function (event) {
+      event.preventDefault();
+
+      var $this = $(this);
+      $this.find('.message:first').text('');
+
+      if ($.trim($("#eventName").val()) === "" || $.trim($("#eventName").val()) === "Event Name" || $.trim($("#eventName").val()) === "Please enter your Event Name") {
+          $("#eventName").val(" Please enter your Event Name");
+          return false;
+      }
+      if ($.trim($("#inputLocation").val()) === "" || $.trim($("#inputLocation").val()) === "Location" || $.trim($("#about Location").val()) === "Please enter information your Location.") {
+          $("#inputLocation").val(" Please enter your event's Location");
+          return false;
+      }
+      if ($.trim($("#inputVenue").val()) === "" || $.trim($("#inputVenue").val()) === "Venue Name" || $.trim($("#inputVenue").val()) === "Please enter your Event Name") {
+          $("#inputVenue").val(" Please enter your Venue Name");
+          return false;
+      }
+      if ($.trim($("#inputDate").val()) === "" || $.trim($("#inputDate").val()) === "MM/DD/YYYY" || $.trim($("#inputDate").val()) === "Please enter a valid Date") {
+          $("#inputDate").val(" Please enter a valid Date");
+          return false;;
+      }
+      if ($.trim($("#ticketCount").val()) === "" || $.trim($("#ticketCount").val()) === "" || $.trim($("#ticketCount").val()) === "Please enter your tickets available") {
+          $("#ticketCount").val(" Please enter your Contact Number");
+          return false;
+      }
+      var newEvents = {
+          event_name: $("#eventName").val().trim(),
+          location_name: $("#inputLocation").val().trim(),
+          venue_name: $("#inputVenue").val().trim(),
+          date: $("#inputDate").val().trim(),
+          ticket_number: $("#ticketCount").val().trim(),
+      };
+
+      console.log(newEvents)
+
+      // Send the POST request.
+      $.ajax("/api/newEvents", {
+          type: "POST",
+          data: newEvents
+      }).then(
+          function (response) {
+              window.location.href = response.redirectTo;
+          },
+          function(error) {
+              console.log(error);
+              $this.find('.message:first').text('An error occured.  Please try again');
+          }
+      );
+  });
+});
--- schema.sql
+++ schema.sql
@@ -1,7 +1,28 @@
--- Drops the ticketDon database if it exists currently --
-DROP DATABASE IF EXISTS ticketDon;
--- Creates the ticketDon database --
-CREATE DATABASE ticketDon;
 
-use ticketDon;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
+
+-- ----------------------------
+--  Table structure for `Events`
+-- ----------------------------
+DROP TABLE IF EXISTS `ticketDon`;
+CREATE TABLE `ticketDon` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_name` varchar(50) NOT NULL,
+  `location_name` varchar(50) NOT NULL,
+  `venue_name` varchar(255) NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `ticket_number` varchar(4),
+  
+  PRIMARY KEY (`id`)
+) 
+
+-- ----------------------------
+--  Test Records
+-- ----------------------------
+BEGIN;
+INSERT INTO `Events` VALUES ('1','Mark', 'Taylor Swift reputation Stadium Tour', 'Philadelphia', 'Lincoln Financial Field', '2018-07-13', '2');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
