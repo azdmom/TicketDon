a015eabf7079773f27b9449287fd244beab16ff0
Dennis <bereznd1@tcnj.edu>
2018-05-22 13:34:18 -0400

    changing routes

--- controllers/apiUserDonor.js
+++ controllers/apiUserDonor.js
@@ -24,7 +24,17 @@ module.exports = function(app) {
     });
 
     //create api/signin route
-
+    $.ajax('/api/signin', {
+      method:'POST',
+      data: {
+        email: email,
+        password: password
+      }
+    }).then(function(response) {
+      sessionStorage.setItem('email', response.email);
+    })
+  
+    const userEmail = sessionStorage.getItem('email');
   });
   
 
--- controllers/apiUserRcpt.js
+++ controllers/apiUserRcpt.js
@@ -26,7 +26,17 @@ module.exports = function(app) {
     });
     
     //create api signin route
-
+    $.ajax('/api/signin', {
+      method:'POST',
+      data: {
+        email: email,
+        password: password
+      }
+    }).then(function(response) {
+      sessionStorage.setItem('email', response.email);
+    })
+  
+    const userEmail = sessionStorage.getItem('email');
   });
   
 
