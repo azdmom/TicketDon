0d8bce40b4b7d43a18e23921afd6040287d3abf6
Drita <ddogjani@gmail.com>
2018-05-23 17:11:46 -0400

    Passport Update

--- controllers/apiTickets.js
+++ controllers/apiTickets.js
@@ -1,12 +1,13 @@
+var path = require("path");
 var express = require("express");
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function(app, passport) {
   // POST route for saving a new event
-  app.post("/api/Tickets", function(req, res) {
+  app.post("/api/Tickets", isLoggedIn, function(req, res) {
     // Add sequelize code for creating a post using req.body,
     // then return the result using res.json
-
+    
     db.userDonors
       .findOne({
         where: {
@@ -33,7 +34,7 @@ module.exports = function(app) {
       });
   });
 
-  app.put("api/ticketRqst", function(req, res){
+  app.put("api/ticketRqst", isLoggedIn, function(req, res){
     db.Tickets.update({
     userRcpts: user.id
   }, {
@@ -44,7 +45,7 @@ module.exports = function(app) {
    });
    
 
-  app.get("/api/Tickets", function(req, res) {
+  app.get("/api/getTickets", isLoggedIn, function(req, res) {
     // Add sequelize code to find all posts, and return them to the user with res.json
     //implement find where cluase where rcpt_id is null
     db.Tickets.findAll({where: {userRcpts: null}
@@ -55,12 +56,24 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/api/ticketEmail", function(req,res){
+  app.get("/api/ticketEmail", isLoggedIn, function(req,res){
   db.Tickets.findAll({where: {[userRcpts.ne]: null}
   }).then(function(user){
     console.log(user)
     res.send(user)
   });
 });
+
+app.get('/logout', function (req, res) {
+  req.session.destroy(function (err) {
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/');
+}
 
 };
--- controllers/apiUserDonor.js
+++ controllers/apiUserDonor.js
@@ -1,15 +1,29 @@
+var path = require("path");
 var express = require("express");
-
 var db = require("../models");
 
-module.exports = function(app) {  
+module.exports = function(app, passport) {  
+
+// register signin route 
+  app.post('/api/donorSignup',
+  passport.authenticate('local-signup', {
+    failWithError: true
+  }),
+  (req, res, next) => {
+    res.status(200).send({
+      redirectTo: "/api/Tickets"
+    });
+  },
+  (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send(err);
+  });
 
   // POST route for saving a new event
-  app.post("/api/userDonor", function(req, res) {
+  app.post("/api/userDonor", isLoggedIn, function(req, res) {
     // Add sequelize code for creating a post using req.body,
     // then return the result using res.json
-
-    db.userDonors.create({
+    var data = {
       email: req.body.email,
       password: req.body.password,
       name: req.body.name,
@@ -18,15 +32,43 @@ module.exports = function(app) {
       city: req.body.city,
       state: req.body.state, 
       zip_code: req.body.zip_code
-
-    }).then(function(dbuserDonors) {
-
+    };
+    db.userDonors.update(data, {
+      where: {
+        id: req.user.id
+      },
+      returning: true,
+      plain: true
+    })
+    .then(function(dbuserDonors) {
       res.json(dbuserDonors);
-      
     });
-
-
   });
   
+  app.post("/api/donorSignin",
+  passport.authenticate("donor-signin", {
+    failWithError: true
+  }),
+  (req, res, next) => {
+    res.status(200).send({
+      redirectTo: "/api/Tickets"
+    });
+  },
+  (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send(err);
+  });
+
+  app.get('/logout', function (req, res) {
+    req.session.destroy(function (err) {
+      res.redirect('/');
+    });
+  });
+  
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+      return next();
+    res.redirect('/');
+  }
 
 };
