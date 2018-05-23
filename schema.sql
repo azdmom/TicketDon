d8110e05459b194e95e0e34b835219dc3fbdc28c
Dennis <bereznd1@tcnj.edu>
2018-05-23 17:45:01 -0400

    events

--- schema.sql
+++ schema.sql
@@ -12,10 +12,6 @@ select * from userdonors;
 
 select * from tickets;
 
-INSERT INTO tickets (event_name, date, location, venue_name, ticket_qty, createdAt, updatedAt, userRcptId, userDonorId )
-VALUES ("jay z", 12/23/1994, "philly", "wells fargo", 5, 12/23/2011, 12/21/2012, 1, 1);
-
-
 
 
 
